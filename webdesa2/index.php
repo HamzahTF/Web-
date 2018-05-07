e3c5ea376faff8813e545d9161c23599f356cdbd
HamzahTF <39054123+HamzahTF@users.noreply.github.com>
2018-05-07 20:47:34 +0700

    WEB
    WEB DINAMIS

---
+++ webdesa2/index.php
@@ -0,0 +1,77 @@
+<?php require 'header.php'; ?>
+
+
+  <div class="vendor row">
+
+      <div style="height:100px;width:250px;background:white;float:left">
+          <div class="left">
+
+            <div class="profilebox">
+                  <div class="centerY">
+                      <div class="circle" style="border:4px solid rgb(61, 214, 151);position:relative">
+                          <div class="centerY" align='center'>
+                              <i class="fa fa-user" style="font-size:24pt;color:rgb(34, 42, 43);"></i>
+                          </div>
+                      </div>
+                       <h6 style="padding:5px">Hi, admin</h6>
+                      <h6 style="padding:0px;"><span style="color:white;background:rgb(61, 214, 151);padding:2.5px;border-radius:5px">Online</span></h6>
+                  </div>
+
+            </div>
+
+            <div class="navigasi">
+                <ul>
+                  <li><a href="index.php">Sejarah Desa</a><span class="icon"><i class="fa fa-Sejarah Desa"></i></span></li>
+                  <li><a href="member.php">Artikel</a><span class="icon"><i class="fa fa-pencil"></i></span></li>
+                </ul>
+            </div>
+
+          </div>
+      </div>
+      <div style="width:calc(100% - 250px);float:left;">
+        <div style="padding:5px 25px">
+          <div class="row">
+            <div style="width:31.3%;color:white;height:120px;background:rgb(255, 78, 78);position:relative;margin:20px 1%;margin-left:0%;margin-right:2%;float:left;box-sizing:border-box;">
+              <i class="fa fa-eye" style="position:absolute;font-size:80pt;opacity:0.2;right:0px;bottom:0px"></i>
+              <div class="centerY">
+<h1 align="center">132.1K</h1>
+<h4 align="center">Pengunjung Web</h4>
+              </div>
+            </div>
+            <div style="width:31.3%;position:relative;color:white;height:120px;background:rgb(78, 133, 255);margin:20px 1%;float:left;box-sizing:border-box;">
+<i class="fa fa-book" style="position:absolute;font-size:80pt;opacity:0.2;right:0px;bottom:0px"></i>
+              <div class="centerY">
+                <h1 align="center">201</h1>
+                <h4 align="center">Projek Desa</h4>
+              </div>
+            </div>
+            <div style="position:relative;width:31.3%;height:120px;color:white;background:rgb(157, 255, 94);margin:20px 1%;margin-left:2%;margin-right:0%;float:left;box-sizing:border-box;">
+              <i class="fa fa-calendar" style="position:absolute;font-size:80pt;opacity:0.2;right:0px;bottom:0px"></i>
+              <div class="centerY">
+                <h1 align="center">120</h1>
+                <h4 align="center">Agenda Desa</h4>
+              </div>
+            </div>
+            <div style="clear:both">
+
+            </div>
+          </div>
+          <div class="box">
+            <h2 style="padding:10px;display:block;border-bottom:1px solid rgba(0, 0, 0, 0.03)">Sejarah Desa</h2>
+            <div style="position:relative;height:300px;">
+              <div class="centerY" align="center">
+                <h2>Selamat Datang Di Web Desa Ampelsari</h2>
+                <p style="padding:5px;">KAMPUNG KECIL NAN INDAH <br> MASA PEMERINTAHAN IMAN SUPARTA TAHUN 1922 S/D TAHUN 1946(KADES ke-1).
+
+Pada masa sebelum Tahun 1921 Desa Ampelsari masih terbelah menjadi dua yaitu Desa Ampel dengan Kepala Desa bernama Mangun Wijaya dan Dessa Pelautan deangan Kepala Desa bernama Tulus.Kemudian karena wilayah kedua Desa tersebut sempit, sehingga tahun 1922 Desa Ampel dan Desa Pelautan digabung menjadi satu desa dan diberi nama Desa Ampelsari dengan dipimpin oleh seorang kepala Desa bernama Iman Suparta. Adapun proses pengangkatan Iman Suparta sebagai Kepala Desa adalah pemilihan langsung tetapi prosesnya dengan cara duduk dimana pada waktu itu ada dua calon kepala desa yaitu Iman Suparta dengan Tulus.Bagi pemilih/warga yang mendukung calon Kepala Desa diantara dua tersebut agar mengikuti duduk semua dibelakang calon Kepala Desa , kemudian dihitung siapa yang paling banyak pengikutnya yang duduk dibelakangnya.Dan kebetulan waktu itu Iman Suparta berhak memimpin Desa Ampelsari untuk yang pertama kali.</p>
+              </div>
+            </div>
+          </div>
+          <p style="font-size:8pt;opacity:0.3;margin:10px 0;">&copy; Hamzah Tokhifur Falakhudin- <?php echo Date('Y') ?> All Rights Reserved</p>
+        </div>
+      </div>
+  </div>
+
+
+
+<?php require 'footer.php'; ?>
